3eb47df09c0a8d58525c42a574597d9947c0073e
Nourhan Fekry <nourhanfekry28>
2021-11-17 09:53:54 +0200

    task 1

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -82,9 +82,6 @@ public class MainActivity extends AppCompatActivity {
     inflater.inflate(R.menu.main, menu);
     return true;
   }
-  // ***
-  // TODO - Task 1 - Show Store Information Activity
-  // ***
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     Intent infoIntent = new Intent(this, InfoActivity.class);
